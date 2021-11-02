719511a1445c6a859c09b158e832ba982d97769d
Teofilo Francisco <teopinillo@gmail.com>
2021-11-02 13:26:03 -0400

    step 1

--- coursera_web-dev/module5-solution/js/script.js
+++ coursera_web-dev/module5-solution/js/script.js
@@ -84,7 +84,7 @@ showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
   buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitly setting the flag to get JSON from server processed into an object literal
+  false); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
@@ -101,7 +101,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
+      var chosenCategoryShortName = chooseRandomCategory ();
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
